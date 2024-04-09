419be981729ba3bcf822a4beba18e84f891f0f2c
Matan Yosef <matan.yosef02@gmail.com>
2024-04-09 23:25:00 +1200

    feat: added animations to clues
    Animation is a slide / card like animation.

--- src/components/Clues.tsx
+++ src/components/Clues.tsx
@@ -1,52 +1,119 @@
 import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, PanInfo } from 'framer-motion';
+import { cn } from '../lib/util.ts';
 import { item } from '../lib/animations.ts';
 
-interface ClueProps {
+interface CluesProps {
     clues: string[];
 }
 
-export default function Clues({ clues }: ClueProps) {
-    const [setCurrentClueIndex, setSetCurrentClueIndex] = useState<number>(0);
+export default function Clues({ clues }: CluesProps) {
+    const [currentClueIndex, setCurrentClueIndex] = useState<number>(0);
 
     useEffect(() => {
         console.log(clues);
         if (!clues) return;
-        setSetCurrentClueIndex(clues.length - 1);
+        setCurrentClueIndex(clues.length - 1);
     }, [clues]);
 
     const nextClue = () => {
-        if (setCurrentClueIndex < clues.length - 1) {
-            setSetCurrentClueIndex(setCurrentClueIndex + 1);
+        if (currentClueIndex < clues.length - 1) {
+            setCurrentClueIndex(currentClueIndex + 1);
         }
     };
 
     const previousClue = () => {
-        if (setCurrentClueIndex > 0) {
-            setSetCurrentClueIndex(setCurrentClueIndex - 1);
+        if (currentClueIndex > 0) {
+            setCurrentClueIndex(currentClueIndex - 1);
+        }
+    };
+
+    const onDragEnd = (
+        _e: MouseEvent | TouchEvent | PointerEvent,
+        { offset, velocity }: PanInfo
+    ) => {
+        if (Math.abs(offset.x) > 300 || Math.abs(velocity.x) > 200) {
+            const direction = offset.x > 0 ? 1 : -1;
+            if (direction === 1) {
+                previousClue();
+            } else {
+                nextClue();
+            }
         }
     };
 
     return (
-        <motion.div
-            variants={item}
-            className="w-full self-start bg-slate-900 p-4 shadow"
-        >
-            <button onClick={previousClue} disabled={setCurrentClueIndex === 0}>
-                Previous
-            </button>
-            <div className="text-white">
-                <span className="font-bold">
-                    Clue #{setCurrentClueIndex + 1}:{' '}
-                </span>
-                {clues[setCurrentClueIndex]}.
+        <motion.div className="w-full" variants={item}>
+            <div className="grid w-full">
+                {clues.map((clue, index) => {
+                    if (
+                        index < currentClueIndex - 1 ||
+                        index > currentClueIndex + 1
+                    ) {
+                        return null;
+                    }
+
+                    return (
+                        <Clue
+                            key={index}
+                            clue={clue}
+                            index={index}
+                            onDragEnd={onDragEnd}
+                            currentDisplayIndex={currentClueIndex}
+                        />
+                    );
+                })}
             </div>
-            <button
-                onClick={nextClue}
-                disabled={setCurrentClueIndex === clues.length - 1}
-            >
-                Next
-            </button>
         </motion.div>
     );
 }
+
+interface ClueProps {
+    onDragEnd: (
+        e: MouseEvent | TouchEvent | PointerEvent,
+        info: PanInfo
+    ) => void;
+    index: number;
+    currentDisplayIndex: number;
+    clue: string;
+}
+
+const Clue = ({ onDragEnd, index, clue, currentDisplayIndex }: ClueProps) => {
+    const isCurrent = currentDisplayIndex === index;
+    const isPrevious = index < currentDisplayIndex;
+    const isNext = index > currentDisplayIndex;
+
+    const animateProps = isCurrent
+        ? { x: 0, scale: 1, opacity: 1 }
+        : {
+              x: isPrevious ? '-5%' : '5%',
+              scale: 0.95,
+              opacity: 0.5,
+          };
+
+    return (
+        <motion.div
+            drag={isCurrent ? 'x' : undefined}
+            dragConstraints={{ left: 0, right: 0 }}
+            onDragEnd={onDragEnd}
+            initial={{
+                x: isPrevious ? '-5%' : '5%',
+                scale: 0.95,
+                opacity: 0,
+            }}
+            animate={animateProps}
+            transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+            className={cn(
+                'col-start-1 col-end-1 row-start-1 row-end-1 w-full self-center overflow-x-hidden bg-slate-900 p-4 text-white shadow',
+                isCurrent
+                    ? 'z-10 cursor-grab justify-self-center'
+                    : '-z-10 w-64 text-ellipsis opacity-50 shadow-xl',
+                isPrevious && 'justify-self-start ',
+                isNext && 'justify-self-end text-ellipsis text-right'
+            )}
+        >
+            <span className={'font-bold'}>Clue #{index + 1}: </span>
+            {clue}.
+        </motion.div>
+    );
+};
