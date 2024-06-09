948a807afe268adea6ad64b6a0317be51c31e4df
Matan Yosef <matan.yosef02@gmail.com>
2024-06-09 15:35:08 +1200

    feat: added animation to word game cards

--- src/components/GalleryCard.tsx
+++ src/components/GalleryCard.tsx
@@ -1,22 +1,29 @@
 import { WordToday } from '../game/game.ts';
 import { formatDateAsMonthDayYear } from '../lib/util.ts';
+import { motion } from 'framer-motion';
+import { forwardRef } from 'react';
 
 interface Props {
     word: WordToday;
     clickHandler: (word: WordToday) => void;
 }
 
-export default function GalleryCard({ word, clickHandler }: Props) {
-    return (
-        <button
-            onClick={() => clickHandler(word)}
-            className="flex aspect-square flex-col items-center justify-center gap-1 rounded-lg border bg-white p-8 shadow-lg"
-        >
-            <h2 className="mb-1 text-2xl font-bold">
-                {formatDateAsMonthDayYear(new Date(word.date))}
-            </h2>
-            <p className="text-sm text-gray-500">Author: {word.author}</p>
-            <p className="text-sm text-gray-500">#{word.id}</p>
-        </button>
-    );
-}
+const GalleryCard = forwardRef<HTMLButtonElement, Props>(
+    ({ word, clickHandler }, ref) => {
+        return (
+            <button
+                ref={ref}
+                onClick={() => clickHandler(word)}
+                className="flex aspect-square flex-col items-center justify-center gap-1 rounded-lg border bg-white p-8 shadow-lg"
+            >
+                <h2 className="mb-1 text-2xl font-bold">
+                    {formatDateAsMonthDayYear(new Date(word.date))}
+                </h2>
+                <p className="text-sm text-gray-500">Author: {word.author}</p>
+                <p className="text-sm text-gray-500">#{word.id}</p>
+            </button>
+        );
+    }
+);
+export default GalleryCard;
+export const GalleryCardMotion = motion(GalleryCard);
--- src/screens/ErrorScreen.tsx
+++ src/screens/ErrorScreen.tsx
@@ -5,7 +5,9 @@ import {
     WordToday,
 } from '../game/game.ts';
 import { useEffect, useState } from 'react';
-import GalleryCard from '../components/GalleryCard.tsx';
+import { GalleryCardMotion } from '../components/GalleryCard.tsx';
+import { AnimatePresence, motion } from 'framer-motion';
+import { galleryCard, previousWordsContainer } from '../lib/animations.ts';
 
 interface ErrorScreenProps {
     error: Error;
@@ -31,49 +33,55 @@ export default function ErrorScreen({ error }: ErrorScreenProps) {
     };
 
     return (
-        <div className="flex h-full w-full  flex-col items-center justify-center gap-4">
-            <h1 className="text-center text-4xl font-extrabold text-slate-900">
-                {isNotFoundError
-                    ? '404 Not Found'
-                    : 'Uh oh... Something went wrong.'}
-            </h1>
-            <p className="text-center text-slate-600">
-                {isNotFoundError
-                    ? "Hmm. It doesn't look like today's word has been set 😅. Apologies — please check back later!"
-                    : 'Please refresh the page to try again.'}
-            </p>
-            <p>
-                Want to submit a word? Get in touch on{' '}
-                <a
-                    href="https://www.linkedin.com/in/matan-yosef-59970b219/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="font-semibold text-amber-500 underline-offset-4 hover:underline"
-                >
-                    LinkedIn
-                </a>
-                .
-            </p>
-            {isNotFoundError && (
-                <>
-                    <p className="py-4 text-gray-400">
-                        or, choose a previous day!
-                    </p>
-                    <div>
-                        {previousWords.length > 0 && (
-                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                                {previousWords.map(word => (
-                                    <GalleryCard
-                                        word={word}
-                                        key={word.date}
-                                        clickHandler={handleCardClick}
-                                    />
-                                ))}
-                            </div>
-                        )}
-                    </div>
-                </>
-            )}
+        <div className="h-full w-full pb-12">
+            <div className="mt-28 flex flex-col items-center justify-center gap-4">
+                <h1 className="text-center text-4xl font-extrabold text-slate-900">
+                    {isNotFoundError
+                        ? '404 Not Found'
+                        : 'Uh oh... Something went wrong.'}
+                </h1>
+                <p className="text-center text-slate-600">
+                    {isNotFoundError
+                        ? "Hmm. It doesn't look like today's word has been set 😅. Apologies — please check back later!"
+                        : 'Please refresh the page to try again.'}
+                </p>
+                <p>
+                    Want to submit a word? Get in touch on{' '}
+                    <a
+                        href="https://www.linkedin.com/in/matan-yosef-59970b219/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="font-semibold text-amber-500 underline-offset-4 hover:underline"
+                    >
+                        LinkedIn
+                    </a>
+                    .
+                </p>
+            </div>
+            <AnimatePresence>
+                {isNotFoundError && previousWords.length > 0 && (
+                    <motion.div
+                        className="flex flex-col items-center justify-center gap-4"
+                        variants={previousWordsContainer}
+                        initial="hidden"
+                        animate="show"
+                    >
+                        <p className="py-4 text-center text-gray-400">
+                            or, choose a previous day!
+                        </p>
+                        <motion.div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                            {previousWords.map(word => (
+                                <GalleryCardMotion
+                                    variants={galleryCard}
+                                    word={word}
+                                    key={word.date}
+                                    clickHandler={handleCardClick}
+                                />
+                            ))}
+                        </motion.div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 }
